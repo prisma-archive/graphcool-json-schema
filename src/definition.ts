1bb86ffe5673c351ad7e2f64a40ac20b66cb25bf
timsuchanek <tim.suchanek@gmail.com>
2017-12-05 14:48:07 +0100

    add 1.0 format

--- src/definition.ts
+++ src/definition.ts
@@ -1,46 +1,29 @@
 export interface GraphcoolDefinition {
-  types: string | string[]
-  permissions?: Array<Permission>
-  functions?: {[name: string]: FunctionDefinition}
-  rootTokens?: string[]
-  custom?: any
-}
-
-export interface Permission {
-  description?: string
-  isEnabled?: boolean
-  // (modelName.(create|read|list|update|delete) | relationName.(connect|disconnect))
-  operation: string
-  authenticated?: boolean
-  query?: string
-  fields?: string[]
-}
-
-export interface FunctionDefinition {
-  handler: FunctionHandler
-  type: FunctionType
-  // (modelName.(create|read|list|update|delete) | relationName.(connect|disconnect))
-  // TODO remove operation, as it is only there to temporarily support the backend
-  operation?: string
-  query?: string
+  service: string
+  datamodel: string | string[]
+  apikey?: string
+  stages: StageMap
   schema?: string
+  custom?: any
+  subscriptions?: SubscriptionMap
 }
 
-export interface FunctionHandler {
-  webhook?: FunctionHandlerWebhookSource
-  code?: FunctionCode | string
+export interface StageMap {
+  [stageName: string]: string
 }
 
-export interface FunctionCode {
-  src: string
-  environment?: {[envVar: string]: string}
+export interface SubscriptionMap {
+  [subscriptionName: string]: SubscriptionDefinition
 }
 
-export interface FunctionHandlerWebhook {
+export interface SubscriptionDefinition {
+  query: string
   webhook: FunctionHandlerWebhookSource
 }
 
-export type FunctionHandlerWebhookSource = string | FunctionHandlerWebhookWithHeaders
+export type FunctionHandlerWebhookSource =
+  | string
+  | FunctionHandlerWebhookWithHeaders
 
 export interface FunctionHandlerWebhookWithHeaders {
   url: string
@@ -51,5 +34,3 @@ export interface Header {
   name: string
   value: string
 }
-
-export type FunctionType = 'operationBefore' | 'operationAfter' | 'subscription' | 'httpRequest' | 'httpResponse' | 'resolver'
